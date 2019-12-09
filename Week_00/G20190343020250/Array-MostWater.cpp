29dbbd93bf6abf32504d8aa2ba1b1b0f584121fa
luhaitian <614073133@qq.com>
2019-12-09 23:11:38 +0800

    test for git

---
+++ Week_00/G20190343020250/Array-MostWater.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i = 0,j = height.size() - 1;
+        int TempS=0,TempMinHeight = 0,TempDistance = 0;
+        int maxArea=0;
+        TempDistance = height.size() - 1;
+        while(i!=j)
+        {       
+            if(height[i]>height[j])
+            {
+                TempMinHeight = height[j];
+                TempS = TempDistance * TempMinHeight; 
+                j--;
+            }
+            else
+            {
+                lalalla
+                TempMinHeight = height[i];
+                TempS = TempDistance * TempMinHeight; 
+                i++;
+            }
+            if(TempS>maxArea)
+                maxArea = TempS;
+            TempDistance--;
+        }
+        return maxArea;
+    }
+};
