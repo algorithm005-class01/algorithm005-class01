3a37f9185e27af6a05ab431c4d67dbbd566957c6
黄雄 <huangxiong@hua-cloud.com.cn>
2019-12-10 15:26:41 +0800

    添加一个在discuss上的简洁写法

--- Week_01/G20190343020110/LeetCode_26_0110.java
+++ Week_01/G20190343020110/LeetCode_26_0110.java
@@ -8,9 +8,10 @@ public class LeetCode_26_0110 {
      * 数组完成排序后，我们可以放置两个指针 ii 和 jj，其中 ii 是慢指针，
      * 而 jj 是快指针。只要 nums[i] = nums[j]nums[i]=nums[j]，我们就增加 jj 以跳过重复项。
      * 当我们遇到 nums[j] \neq nums[i]nums[j]
-     *  =nums[i] 时，跳过重复项的运行已经结束，因此我们必须把它（nums[j]nums[j]）的值复制到
-     *  nums[i + 1]nums[i+1]。然后递增 ii，接着我们将再次重复相同的过程，
-     *  直到 jj 到达数组的末尾为止。
+     * =nums[i] 时，跳过重复项的运行已经结束，因此我们必须把它（nums[j]nums[j]）的值复制到
+     * nums[i + 1]nums[i+1]。然后递增 ii，接着我们将再次重复相同的过程，
+     * 直到 jj 到达数组的末尾为止。
+     *
      * @param nums
      * @return
      */
@@ -20,10 +21,23 @@ public class LeetCode_26_0110 {
         }
         int j = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != nums[j]){
+            if (nums[i] != nums[j]) {
                 nums[++j] = nums[i];
             }
         }
         return ++j;
+    }
+
+    /**
+     * 这个写法更为简洁，国际站的Discuss上的
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates2(int[] nums) {
+        int cur = 0;
+        for (int n : nums)
+            if (n > nums[cur])
+                nums[++cur] = n;
+        return cur + 1;
     }
 }
