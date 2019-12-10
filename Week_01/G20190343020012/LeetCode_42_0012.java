5332c15d7d94683c0718726c2abaf05179bca661
Feng Zhang <fezhang@thoughtworks.com>
2019-12-10 15:16:19 +0800

    add week_01 code

---
+++ Week_01/G20190343020012/LeetCode_42_0012.java
@@ -0,0 +1,20 @@
+
+public class Solution42 {
+  public int trap(int[] height) {
+    int ans = 0;
+    int size = height.length;
+    for (int i = 1; i < size - 1; i++) {
+      int max_left = 0;
+      int max_right = 0;
+      for (int j = i; j >= 0; j--) {
+        max_left = Math.max(max_left, height[j]);
+      }
+      for (int j = i; j < size; j++) {
+        max_right = Math.max(max_right, height[j]);
+      }
+      ans += Math.min(max_left, max_right) - height[i];
+    }
+    return ans;
+  }
+
+}
