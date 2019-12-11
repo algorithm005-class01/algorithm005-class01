8e43d4ab2649819e95664f0e84a5265d32c80a8c
wufeng <t3476t3485bao2@163.com>
2019-12-11 12:00:45 +0800

    提交最小栈解法

--- Week_01/G20190343020184/LeetCode_42_0184.cc
+++ Week_01/G20190343020184/LeetCode_42_0184.cc
@@ -1,8 +1,12 @@
 #include <algorithm>
 #include <iostream>
+#include <stack>
 #include <vector>
 using namespace std;
 
+/*
+ *自己做的，预处理数组
+ */
 int trap(vector<int>& height)
 {
     if (height.size() == 0)
@@ -26,6 +30,32 @@ int trap(vector<int>& height)
 	res += max(min(left, right) - height[i], 0);
     }
     return res;
+}
+
+/*
+ *这道题要求求每一个柱子左右两边最长的柱子，感觉用预处理数组就很好了。
+ *下面用栈来做。默写答案版本
+*/
+
+int trap2(vector<int>& height)
+{
+    int ans = 0;
+    int current = 0;
+    stack<int> sta;
+    while (current < height.size()) {
+	while (!sta.empty() && height[current] > height[sta.top()]) {
+	    int top = sta.top();
+	    sta.pop();
+	    if (sta.empty())
+		break;
+	    int distance = current - sta.top() - 1;
+	    int bounded_height = min(height[current], height[sta.top()]) - height[top];
+	    ans += distance * bounded_height;
+	}
+	sta.push(current);
+	++current;
+    }
+    return ans;
 }
 
 int main(int argc, char* argv[])
