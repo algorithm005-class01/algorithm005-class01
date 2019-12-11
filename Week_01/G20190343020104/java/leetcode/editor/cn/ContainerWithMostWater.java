4ef04971c6b3416a2f0d7b788f9a5007add36e2e
heshun <heshun@com.centnet.cn>
2019-12-11 23:40:32 +0800

    双向夹逼

--- Week_01/G20190343020104/java/leetcode/editor/cn/ContainerWithMostWater.java
+++ Week_01/G20190343020104/java/leetcode/editor/cn/ContainerWithMostWater.java
@@ -27,17 +27,18 @@ public class ContainerWithMostWater {
     class Solution {
         /**
          * 1.暴力法，双重循环，挨个比较最大区域。
-         *
+         * 2.双向夹逼法
          * @param height
          * @return
          */
         public int maxArea(int[] height) {
             int max = 0;
-            for (int i = 0; i < height.length - 1; i++) {
-                for (int j = i + 1; j < height.length; j++) {
-                    System.out.println(height[i] + " & " + height[j]);
-                    max = Math.max(max, (j - i) * Math.min(height[i], height[j]));
-                }
+            for (int i = 0, j = height.length - 1; i < j; ) {
+                System.out.println(height[i] + " * " + height[j]);
+                int minHeight = height[i] < height[j] ? height[i++] : height[j--];
+                int area = (j - i + 1) * minHeight;
+                max = Math.max(max, area);
+
             }
             return max;
 
