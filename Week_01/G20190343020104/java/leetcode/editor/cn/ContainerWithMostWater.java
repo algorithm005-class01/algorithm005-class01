390bc5a75dfa1be6c7b8c4f1ce3960378cfbf015
heshun <heshun@com.centnet.cn>
2019-12-11 23:31:12 +0800

    暴力法

---
+++ Week_01/G20190343020104/java/leetcode/editor/cn/ContainerWithMostWater.java
@@ -0,0 +1,47 @@
+//给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。 
+//
+// 说明：你不能倾斜容器，且 n 的值至少为 2。 
+//
+// 
+//
+// 图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。 
+//
+// 
+//
+// 示例: 
+//
+// 输入: [1,8,6,2,5,4,8,3,7]
+//输出: 49 
+// Related Topics 数组 双指针
+
+package leetcode.editor.cn;
+
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+        Solution solution = new ContainerWithMostWater().new Solution();
+        System.out.println(solution.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+    }
+
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        /**
+         * 1.暴力法，双重循环，挨个比较最大区域。
+         *
+         * @param height
+         * @return
+         */
+        public int maxArea(int[] height) {
+            int max = 0;
+            for (int i = 0; i < height.length - 1; i++) {
+                for (int j = i + 1; j < height.length; j++) {
+                    max = Math.max(max, (j - i) * Math.min(height[i], height[j]));
+                }
+            }
+            return max;
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
