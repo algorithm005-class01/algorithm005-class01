da453f03bcb94723dcbf3de50da0b30850267ff6
heshun <heshun@content.com.cn>
2019-12-13 22:48:40 +0800

    还有问题，输出了重复的数组

--- Week_01/G20190343020104/java/leetcode/editor/cn/ThreeSum.java
+++ Week_01/G20190343020104/java/leetcode/editor/cn/ThreeSum.java
@@ -34,11 +34,15 @@ public class ThreeSum {
          */
         public List<List<Integer>> threeSum(int[] nums) {
             List<List<Integer>> result = new ArrayList<>();
-            for (int i = 0; i < nums.length; i++) {
-                for (int j = i + 1; j < nums.length; j++) {
+            for (int i = 0; i < nums.length - 2; i++) {
+                for (int j = i + 1; j < nums.length - 1; j++) {
                     for (int k = j + 1; k < nums.length; k++) {
                         if (nums[i] + nums[j] + nums[k] == 0) {
-                            result.add(List.of(nums[i], nums[j], nums[k]));
+                            List<Integer> list = new ArrayList<Integer>();
+                            list.add(nums[i]);
+                            list.add(nums[j]);
+                            list.add(nums[k]);
+                            result.add(list);
                         }
                     }
                 }
