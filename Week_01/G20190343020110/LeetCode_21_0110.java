053392e97ad1b259bf852bb5bcd30d6e677f2c2a
talen0702 <40754378+talen0702@users.noreply.github.com>
2019-12-14 07:59:42 +0800

    题目21

---
+++ Week_01/G20190343020110/LeetCode_21_0110.java
@@ -0,0 +1,58 @@
+import java.util.LinkedList;
+
+/**
+ * @Author huangxiong
+ * @Date 2019/12/12
+ * @Description leectcode题目21 合并两个链表
+ **/
+public class LeetCode_21_0110 {
+    public synchronized ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists1(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists1(l1, l2.next);
+            return l2;
+        }
+    }
+
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode result = new ListNode(0);
+        ListNode prev = result;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        if (l1 != null) {
+            prev.next = l1;
+        }
+        if (l2 != null) {
+            prev.next = l2;
+        }
+        return result.next;
+    }
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.remove(1);
+        LeetCode_21_0110 code_21 = new LeetCode_21_0110();
+        System.out.println(code_21.mergeTwoLists1(null, null));
+    }
+}
