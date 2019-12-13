c71f6f3148d403bd1168da724ebaa0d7b5d55b9e
fageiguanbing <fageiguanbing@gmail.com>
2019-12-13 10:06:15 +0800

    week 1

---
+++ Week_01/G20190343020032/LeetCode_26_0032.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int index = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[index]) {
+                nums[++index] = nums[i];
+            }
+        }
+    
+        return index + 1;
+    }
+}
