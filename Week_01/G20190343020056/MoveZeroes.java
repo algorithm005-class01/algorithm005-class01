608155f5e2fbb22f39832964b47367f6da63798f
xiaomingye <xiaomingye@bone.com>
2019-12-16 00:11:54 +0800

    0056_Week01

--- Week_01/G20190343020056/MoveZeroes.java
+++ Week_01/G20190343020056/MoveZeroes.java
@@ -8,6 +8,7 @@ class Solution {
                 nums[i - count] = nums[i]; //前移的思想，前移的位数为count，有几个 0 ，就向前面移动几位
             }
         }
+        
         for (int i = nums.length - count; i < nums.length; i++) {
             nums[i] = 0;
         }
