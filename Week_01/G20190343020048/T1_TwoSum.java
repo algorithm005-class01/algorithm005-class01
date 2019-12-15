b71aa38beafe929a78d841caafea972bc1add625
wenzhuang <240847984@qq.com>
2019-12-15 16:52:13 +0800

    第3课作业题

---
+++ Week_01/G20190343020048/T1_TwoSum.java
@@ -0,0 +1,25 @@
+package main.leetcode_solutions.array;
+
+import java.util.*;
+
+/**
+ * @author wenzhuang
+ * @date 2019-12-15
+ */
+public class T1_TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            if (map.containsKey(target - nums[i])){
+                res[0] = map.get(target - nums[i]);
+                res[1] = i;
+                return res;
+            }
+            map.put(nums[i], i);
+        }
+        return res;
+    }
+
+}
