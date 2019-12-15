ee503156d3ac439a76f058dceb3e9380919819dc
Ben <ben@zfben.com>
2019-12-15 21:25:11 +0800

    add class 4

--- Week_01/G20190343020188/LeetCode_42_0188.js
+++ Week_01/G20190343020188/LeetCode_42_0188.js
@@ -3,6 +3,8 @@
  * @param {number[]} height
  * @return {number}
  */
+
+// 栈
 var trap = function(height) {
   var ans = 0;
   var current = 0;
@@ -17,6 +19,26 @@ var trap = function(height) {
       ans += distance * bounded_height;
     }
     stack.unshift(current++);
+  }
+  return ans;
+};
+
+// 双指针
+var trap = function(height) {
+  var left = 0;
+  var right = height.length - 1;
+  var ans = 0;
+  var left_max = 0;
+  var right_max = 0;
+  while (left < right) {
+    if (height[left] < height[right]) {
+      height[left] >= left_max ? (left_max = height[left]) : ans += (left_max - height[left]);
+        ++left;
+    }
+    else {
+      height[right] >= right_max ? (right_max = height[right]) : ans += (right_max - height[right]);
+        --right;
+    }
   }
   return ans;
 };
