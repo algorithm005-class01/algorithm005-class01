d21062cfefc61f56d461bb9b18701dad79ef797d
wangyongtian <ten951@qq.com>
2019-12-17 15:45:33 +0800

    二叉树中序遍历

---
+++ Week_02/G20190343020220/leetcode_94_0220/Sulotion.java
@@ -0,0 +1,70 @@
+package G20190343020220.leetcode_94_0220;
+
+/*
+* 给定一个二叉树，返回它的中序 遍历。
+
+示例:
+
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+输出: [1,3,2]
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+*/
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 二叉树的中序遍历
+ *
+ * @author Darcy
+ * @date 2019-12-17 14:14
+ */
+public class Sulotion {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.addFirst(curr);
+                curr = curr.left;
+            }
+            curr = stack.removeFirst();
+            res.add(curr.val);
+            curr = curr.right;
+
+        }
+        return res;
+
+    }
+
+    List<Integer> res = new ArrayList<>();
+
+    public List<Integer> inorderTraversal1(TreeNode root) {
+        if (root == null) {
+            return res;
+        }
+        inorderTraversal(root.left);
+        res.add(root.val);
+        inorderTraversal(root.right);
+        return res;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
