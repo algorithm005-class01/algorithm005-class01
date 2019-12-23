0f70aeda63db6d5694f1f937f77652ca49f763a8
xz <zjulxz@gmail.com>
2019-12-23 16:38:11 +0800

    update leetcode 017

--- Week_03/G20190343020044/leetcode-017.py
+++ Week_03/G20190343020044/leetcode-017.py
@@ -1,22 +1,5 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if digits == "":
-            return []
-        if len(digits) == 1:
-            return self.letters(digits)
-        else:
-            sub = digits[:-1]
-            res = self.addone(digits[-1], self.letterCombinations(sub))
-            return res
-
-    def addone(self, digit: str, prefix: List[str]) -> List[str]:
-        res = []
-        for ch in self.letters(digit):
-            for s in prefix:
-                res.append(s + ch)
-        return res
-    
-    def letters(self, digit: str) -> List[str]:
         phone = {'2': ['a', 'b', 'c'],
                  '3': ['d', 'e', 'f'],
                  '4': ['g', 'h', 'i'],
@@ -25,4 +8,14 @@ class Solution:
                  '7': ['p', 'q', 'r', 's'],
                  '8': ['t', 'u', 'v'],
                  '9': ['w', 'x', 'y', 'z']}
-        return phone[digit]
+        if not digits: return []
+        if len(digits) == 1: return phone[digits]
+        
+        def addone(digit: str, prefix: List[str]) -> List[str]:
+            res = []
+            for ch in phone[digit]:
+                for s in prefix:
+                    res.append(s + ch)
+            return res
+        sub = digits[:-1]
+        return addone(digits[-1], self.letterCombinations(sub))
