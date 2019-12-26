6489170796124c135ae0016065b57e80e28b1d76
talen0702 <40754378+talen0702@users.noreply.github.com>
2019-12-26 19:01:58 +0800

    多数元素,添加排序和分治解法

--- Week_03/G20190343020110/LeetCode_169_0110.java
+++ Week_03/G20190343020110/LeetCode_169_0110.java
@@ -5,6 +5,12 @@
  **/
 public class LeetCode_169_0110 {
 
+    /**
+     * 暴力遍历法
+     *
+     * @param nums
+     * @return
+     */
     public int majorityElement(int[] nums) {
         int majorityCount = nums.length / 2;
         for (int num : nums) {
@@ -20,5 +26,50 @@ public class LeetCode_169_0110 {
 
         }
         return -1;
+    }
+
+    /**
+     * 排序法
+     *
+     * @param nums
+     * @return
+     */
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+
+
+    private int randRange(Random rand, int min, int max) {
+        return rand.nextInt(max - min) + min;
+    }
+
+    private int countOccurences(int[] nums, int num) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == num) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * 分治法
+     *
+     * @param nums
+     * @return
+     */
+    public int majorityElement(int[] nums) {
+        Random rand = new Random();
+
+        int majorityCount = nums.length / 2;
+
+        while (true) {
+            int candidate = nums[randRange(rand, 0, nums.length)];
+            if (countOccurences(nums, candidate) > majorityCount) {
+                return candidate;
+            }
+        }
     }
 }
