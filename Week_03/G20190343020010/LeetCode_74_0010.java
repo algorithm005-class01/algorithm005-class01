2bdff71083ea4e838dea6b84b60d861410881491
xiezhilin <xiezhilin@xiaomi.com>
2019-12-29 15:59:41 +0800

    HomeWork_03_0010

---
+++ Week_03/G20190343020010/LeetCode_74_0010.java
@@ -0,0 +1,58 @@
+package com.android.browser.report;
+
+/**
+ * 编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+ * <p>
+ * 每行中的整数从左到右按升序排列。
+ * 每行的第一个整数大于前一行的最后一个整数。
+ * 示例 1:
+ * <p>
+ * 输入:
+ * matrix = [
+ * [1,   3,  5,  7],
+ * [10, 11, 16, 20],
+ * [23, 30, 34, 50]
+ * ]
+ * target = 3
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:
+ * matrix = [
+ * [1,   3,  5,  7],
+ * [10, 11, 16, 20],
+ * [23, 30, 34, 50]
+ * ]
+ * target = 13
+ * 输出: false
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-a-2d-matrix
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode_74_0010 {
+
+    class Solution {
+        public boolean searchMatrix(int[][] matrix, int target) {
+            int m = matrix.length;
+            if (m == 0) return false;
+            int n = matrix[0].length;
+
+            // 二分查找
+            int left = 0, right = m * n - 1;
+            int pivotIdx, pivotElement;
+            while (left <= right) {
+                pivotIdx = (left + right) / 2;
+                pivotElement = matrix[pivotIdx / n][pivotIdx % n];
+                if (target == pivotElement) return true;
+                else {
+                    if (target < pivotElement) right = pivotIdx - 1;
+                    else left = pivotIdx + 1;
+                }
+            }
+            return false;
+        }
+    }
+
+
+}
