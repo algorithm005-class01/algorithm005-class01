54ede1d5d5f5c9667451c895fd785c259fed018c
yindongqing <yindongqing1128@126.com>
2019-12-29 20:44:02 +0800

    homework for week 3

---
+++ Week_03/G20190343020350/week3/leetcode-169-majority-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int target = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (target != nums[i]) {
+                count--;
+                if (count == 0) {
+                    target = nums[i];
+                    count = 1;
+                }
+            }
+            else {
+                count++;
+            }
+        }
+        return target;
+    }
+};
