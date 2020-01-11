ccef7601208d6b6da3db0113a0e88a0fceadd5a0
talen0702 <hxdream_sz@126.com>
2020-01-11 21:52:56 +0800

    任务调度器

---
+++ Week_05/G20190343020110/LeetCode_621_0110.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+/**
+ * @Author huangxiong
+ * @Date 2020/1/11
+ * @Description
+ **/
+public class LeetCode_621_0110 {
+    public int leastInterval1(char[] tasks, int n) {
+        int[] map = new int[26];
+        for (char c : tasks)
+            map[c - 'A']++;
+        Arrays.sort(map);
+        int max_val = map[25] - 1, idle_slots = max_val * n;
+        for (int i = 24; i >= 0 && map[i] > 0; i--) {
+            idle_slots -= Math.min(map[i], max_val);
+        }
+        return idle_slots > 0 ? idle_slots + tasks.length : tasks.length;
+    }
+
+    public int leastInterval2(char[] tasks, int n) {
+        int[] map = new int[26];
+        for (char c : tasks)
+            map[c - 'A']++;
+        Arrays.sort(map);
+        int time = 0;
+        while (map[25] > 0) {
+            int i = 0;
+            while (i <= n) {
+                if (map[25] == 0)
+                    break;
+                if (i < 26 && map[25 - i] > 0)
+                    map[25 - i]--;
+                time++;
+                i++;
+            }
+            Arrays.sort(map);
+        }
+        return time;
+    }
+}
