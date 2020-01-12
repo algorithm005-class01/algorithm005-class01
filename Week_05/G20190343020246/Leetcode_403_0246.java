bf4bc27beb51874bb608de9f60d3a02bfe1cd378
raysonfang <793514387@qq.com>
2020-01-12 23:30:38 +0800

    0246_week 05

---
+++ Week_05/G20190343020246/Leetcode_403_0246.java
@@ -0,0 +1,47 @@
+/**
+ * 403. 青蛙过河
+ * 一只青蛙想要过河。 假定河流被等分为 x 个单元格，并且在每一个单元格内都有可能放有一石子（也有可能没有）。 青蛙可以跳上石头，但是不可以跳入水中。
+ *
+ * 给定石子的位置列表（用单元格序号升序表示）， 请判定青蛙能否成功过河（即能否在最后一步跳至最后一个石子上）。 开始时， 青蛙默认已站在第一个石子上，并可以假定它第一步只能跳跃一个单位（即只能从单元格1跳至单元格2）。
+ *
+ * 如果青蛙上一步跳跃了 k 个单位，那么它接下来的跳跃距离只能选择为 k - 1、k 或 k + 1个单位。 另请注意，青蛙只能向前方（终点的方向）跳跃。
+ *
+ * 请注意：
+ *
+ * 石子的数量 ≥ 2 且 < 1100；
+ * 每一个石子的位置序号都是一个非负整数，且其 < 231；
+ * 第一个石子的位置永远是0。
+ *
+ */
+class Solution {
+    public boolean canCross(int[] stones) {
+        if (stones.length == 0) {
+            return true;
+        }
+
+        HashMap<Integer, HashSet<Integer>> map = new HashMap<Integer, HashSet<Integer>>(stones.length);
+        map.put(0, new HashSet<Integer>());
+        map.get(0).add(1);
+        for (int i = 1; i < stones.length; i++) {
+            map.put(stones[i], new HashSet<Integer>() );
+        }
+
+        for (int i = 0; i < stones.length - 1; i++) {
+            int stone = stones[i];
+            for (int step : map.get(stone)) {
+                int reach = step + stone;
+                if (reach == stones[stones.length - 1]) {
+                    return true;
+                }
+                HashSet<Integer> set = map.get(reach);
+                if (set != null) {
+                    set.add(step);
+                    if (step - 1 > 0) set.add(step - 1);
+                    set.add(step + 1);
+                }
+            }
+        }
+
+        return false;
+    }
+}
