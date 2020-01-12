9aededa7c3067b7de2995d19929493db8aaecc8e
nightfallsad <nightfallsad@gmail.com>
2020-01-12 23:55:38 +0800

    0342_Week 05

---
+++ Week_05/G20190343020342/LeetCode_32_longest-valid-parentheses_0342-2.js
@@ -0,0 +1,19 @@
+// 解法二：动态规划
+// 时间复杂度：O(n)
+// 空间复杂度：O(n)
+var longestValidParentheses = function(s) {
+    var max = 0;
+    var n = s.length;
+    var dp = new Array(n).fill(0);
+    for (var i = 1; i < n; i++){
+       if (s[i] == ')') {
+            if (s[i-1] == '(') {
+               dp[i] = ( i >= 2 ? dp[i-2] : 0) + 2;
+            } else if (i - dp[i-1] > 0 && s[i - dp[i-1] - 1] == '(') {
+               dp[i] = dp[i-1] + ( (i - dp[i-1] >= 2) ? dp[i - dp[i-1] - 2] : 0 ) + 2;
+            }
+            max = Math.max(max,dp[i]);
+       }
+    }
+    return max;
+};
