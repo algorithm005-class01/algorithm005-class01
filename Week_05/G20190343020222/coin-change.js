deed91a774f4635dd26780c32cc8081b1cf44ad4
Yuxin Pan <ypan8@dons.usfca.edu>
2020-01-12 23:32:10 +0800

    0222_Week 05

---
+++ Week_05/G20190343020222/coin-change.js
@@ -0,0 +1,14 @@
+var coinChange = function(coins, amount) {
+    const dp = (new Array(amount + 1)).fill(amount + 1);
+    dp[0] = 0;
+    
+    for (let i = 1; i <= amount; i++) {
+        for (let j = 0; j < coins.length; j++) {
+            if (coins[j] <= i) {
+                dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
+    }
+    
+    return dp[amount] > amount ? -1 : dp[amount];
+};
