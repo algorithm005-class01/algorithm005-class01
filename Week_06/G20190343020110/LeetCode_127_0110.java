88ce5c8a1c28b1b2bac1f6680638ede61792fe9a
talen0702 <hxdream_sz@126.com>
2020-01-18 14:51:48 +0800

    单词接龙

---
+++ Week_06/G20190343020110/LeetCode_127_0110.java
@@ -0,0 +1,94 @@
+import javafx.util.Pair;
+
+import java.util.*;
+
+/**
+ * @Author huangxiong
+ * @Date 2020/01/18
+ * @Description
+ **/
+public class LeetCode_127_0110 {
+    /**
+     *
+     算法
+
+     对给定的 wordList 做预处理，找出所有的通用状态。将通用状态记录在字典中，键是通用状态，值是所有具有通用状态的单词。
+
+     将包含 beginWord 和 1 的元组放入队列中，1 代表节点的层次。我们需要返回 endWord 的层次也就是从 beginWord 出发的最短距离。
+
+     为了防止出现环，使用访问数组记录。
+
+     当队列中有元素的时候，取出第一个元素，记为 current_word。
+
+     找到 current_word 的所有通用状态，并检查这些通用状态是否存在其它单词的映射，这一步通过检查 all_combo_dict 来实现。
+
+     从 all_combo_dict 获得的所有单词，都和 current_word 共有一个通用状态，所以都和 current_word 相连，因此将他们加入到队列中。
+
+     对于新获得的所有单词，向队列中加入元素 (word, level + 1) 其中 level 是 current_word 的层次。
+
+     最终当你到达期望的单词，对应的层次就是最短变换序列的长度。
+
+     标准广度优先搜索的终止条件就是找到结束单词。
+     * @param beginWord
+     * @param endWord
+     * @param wordList
+     * @return
+     */
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        // Since all words are of same length.
+        int L = beginWord.length();
+
+        // Dictionary to hold combination of words that can be formed,
+        // from any given word. By changing one letter at a time.
+        HashMap<String, ArrayList<String>> allComboDict = new HashMap<String, ArrayList<String>>();
+
+        wordList.forEach(
+                word -> {
+                    for (int i = 0; i < L; i++) {
+                        // Key is the generic word
+                        // Value is a list of words which have the same intermediate generic word.
+                        String newWord = word.substring(0, i) + '*' + word.substring(i + 1, L);
+                        ArrayList<String> transformations =
+                                allComboDict.getOrDefault(newWord, new ArrayList<String>());
+                        transformations.add(word);
+                        allComboDict.put(newWord, transformations);
+                    }
+                });
+
+        // Queue for BFS
+        Queue<Pair<String, Integer>> Q = new LinkedList<Pair<String, Integer>>();
+        Q.add(new Pair(beginWord, 1));
+
+        // Visited to make sure we don't repeat processing same word.
+        HashMap<String, Boolean> visited = new HashMap<String, Boolean>();
+        visited.put(beginWord, true);
+
+        while (!Q.isEmpty()) {
+            Pair<String, Integer> node = Q.remove();
+            String word = node.getKey();
+            int level = node.getValue();
+            for (int i = 0; i < L; i++) {
+
+                // Intermediate words for current word
+                String newWord = word.substring(0, i) + '*' + word.substring(i + 1, L);
+
+                // Next states are all the words which share the same intermediate state.
+                for (String adjacentWord : allComboDict.getOrDefault(newWord,
+                        new ArrayList<String>())) {
+                    // If at any point if we find what we are looking for
+                    // i.e. the end word - we can return with the answer.
+                    if (adjacentWord.equals(endWord)) {
+                        return level + 1;
+                    }
+                    // Otherwise, add it to the BFS Queue. Also mark it visited
+                    if (!visited.containsKey(adjacentWord)) {
+                        visited.put(adjacentWord, true);
+                        Q.add(new Pair(adjacentWord, level + 1));
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+}
