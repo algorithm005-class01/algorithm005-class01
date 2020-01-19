600deba9f21a0b89ccbf9cdd88f03a9f81f0ca5c
“chaihuo” <bravetsia@gmail.com>
2020-01-19 21:29:36 +0800

    0270_Week 06

---
+++ Week_06/G20190343020270/Implement Trie (Prefix Tree).py
@@ -0,0 +1,64 @@
+# 208. Implement Trie (Prefix Tree)
+
+# Implement a trie with insert, search, and startsWith methods.
+
+# Example:
+
+# Trie trie = new Trie();
+
+# trie.insert("apple");
+# trie.search("apple");   // returns true
+# trie.search("app");     // returns false
+# trie.startsWith("app"); // returns true
+# trie.insert("app");   
+# trie.search("app");     // returns true
+
+# Note:
+
+# You may assume that all inputs are consist of lowercase letters a-z.
+# All inputs are guaranteed to be non-empty strings.
+
+class Trie:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.lookup = {}
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        tree = self.lookup
+        for a in word:
+            if a not in tree:
+                tree[a] = {}
+            tree = tree[a]
+        # 单词结束标志
+        tree["#"] = "#"
+        
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        tree = self.lookup
+        for a in word:
+            if a not in tree:
+                return False
+            tree = tree[a]
+        if "#" in tree:
+            return True
+        return False
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        tree = self.lookup
+        for a in prefix:
+            if a not in tree:
+                return False
+            tree = tree[a]
+        return True
