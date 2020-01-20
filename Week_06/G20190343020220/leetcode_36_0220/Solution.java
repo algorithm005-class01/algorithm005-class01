8517caca422f9f7d26e5846543530183a468907d
wangyongtian <ten951@qq.com>
2020-01-20 12:40:14 +0800

    36. 有效的数独

--- Week_06/G20190343020220/leetcode_36_0220/Solution.java
+++ Week_06/G20190343020220/leetcode_36_0220/Solution.java
@@ -1,5 +1,6 @@
 package Week_06.G20190343020220.leetcode_36_0220;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -27,9 +28,9 @@ public class Solution {
 
     public boolean isValidSudoku1(char[][] board) {
         for (int i = 0; i < 9; i++) {
-            HashSet<Character> rows = new HashSet<>();
-            HashSet<Character> columns = new HashSet<>();
-            HashSet<Character> cube = new HashSet<>();
+            HashSet<Character> rows = new HashSet<>(9);
+            HashSet<Character> columns = new HashSet<>(9);
+            HashSet<Character> cube = new HashSet<>(9);
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] != '.' && !rows.add(board[i][j])) {
                     return false;
@@ -44,6 +45,40 @@ public class Solution {
                 }
             }
         }
+        return true;
+    }
+
+    public boolean isValidSudoku2(char[][] board) {
+        // init data
+        HashMap<Integer, Integer>[] rows = new HashMap[9];
+        HashMap<Integer, Integer> [] columns = new HashMap[9];
+        HashMap<Integer, Integer> [] boxes = new HashMap[9];
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashMap<Integer, Integer>();
+            columns[i] = new HashMap<Integer, Integer>();
+            boxes[i] = new HashMap<Integer, Integer>();
+        }
+
+        // validate a board
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+                if (num != '.') {
+                    int n = (int)num;
+                    int box_index = (i / 3 ) * 3 + j / 3;
+
+                    // keep the current cell value
+                    rows[i].put(n, rows[i].getOrDefault(n, 0) + 1);
+                    columns[j].put(n, columns[j].getOrDefault(n, 0) + 1);
+                    boxes[box_index].put(n, boxes[box_index].getOrDefault(n, 0) + 1);
+
+                    // check if this value has been already seen before
+                    if (rows[i].get(n) > 1 || columns[j].get(n) > 1 || boxes[box_index].get(n) > 1)
+                        return false;
+                }
+            }
+        }
+
         return true;
     }
 
