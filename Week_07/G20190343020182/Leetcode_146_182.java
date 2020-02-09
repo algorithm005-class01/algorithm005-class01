12dfd6e93630ed0de1d1720a2edfcde8f1b5eb75
handylin <51332lin@gmail.com>
2020-02-09 14:30:18 +0800

    Week_07

---
+++ Week_07/G20190343020182/Leetcode_146_182.java
@@ -0,0 +1,28 @@
+package week007;
+
+import org.junit.Test;
+
+/*
+编写�?个函数，输入是一个无符号整数，返回其二进制表达式中数字位数为 �?1�? 的个数（也被称为汉明重量�?
+ * */
+public class Leetcode_146_182 {
+
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        int tmp = n;
+        while (tmp != 0) {
+        	tmp = tmp & (tmp - 1);
+        	count++;
+        }
+        return count;
+    }
+    
+    
+    @Test
+    public void test()
+    {
+    	System.out.println(hammingWeight(5));    
+    	System.out.println(hammingWeight(Integer.MAX_VALUE)); 
+    }
+}
