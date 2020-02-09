9ca1b6c95f5b9e7d3c634e89a5438c34308bf4d9
yx101 <quyixia@icloud.com>
2020-02-09 19:29:19 +0800

    0106_Week 07 Java (+ Week 06 请假补交)

---
+++ Week_07/G20190343020106/LeetCode_242_0106.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.isEmpty() || t.isEmpty()) {
+            return true;
+        }
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] sArr = new int[26];
+        int[] tArr = new int[26];
+
+        for (int i = 0; i < t.length(); i++) {
+            ++sArr[s.charAt(i) - 'a'];
+            ++tArr[t.charAt(i) - 'a'];
+        }
+        for (int i = 0; i < 26; i++) {
+            if (sArr[i] != tArr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
