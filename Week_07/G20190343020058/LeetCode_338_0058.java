ad13c40c50b88dfd330dd9c010010a77b7fe38fb
啸风 <57555536+xfengc@users.noreply.github.com>
2020-02-09 17:31:24 +0800

    0058_Week 07
    0058_Week 07

---
+++ Week_07/G20190343020058/LeetCode_338_0058.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+        for (int i = 1; i <= num; ++i)
+          ans[i] = ans[i >> 1] + (i & 1);  
+        return ans;
+    }
+  }
