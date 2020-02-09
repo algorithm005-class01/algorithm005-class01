e545e82f578f743f7a2abf743fdb4025629d7bd0
fuxuejiang <39236231+fuxuejiang@users.noreply.github.com>
2020-02-09 18:32:02 +0800

    Create LeetCode_146_0074.java

---
+++ Week_07/G20190343020074/LeetCode_146_0074.java
@@ -0,0 +1,28 @@
+class LRUCache extends LinkedHashMap<Integer, Integer>{
+    private int capacity;
+    
+    public LRUCache(int capacity) {
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity; 
+    }
+}
+
+/**
+ * LRUCache 对象会以如下语句构造和调用:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
