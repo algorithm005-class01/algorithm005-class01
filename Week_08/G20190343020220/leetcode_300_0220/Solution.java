1e80950ae3ac1b4e97f9b55a1652364a33379336
wangyongtian <wangyongtian@daojia-inc.com>
2020-02-16 20:09:39 +0800

    7

---
+++ Week_08/G20190343020220/leetcode_300_0220/Solution.java
@@ -0,0 +1,23 @@
+package Week_08.G20190343020220.leetcode_300_0220;
+
+/**
+ * @author Darcy
+ * @date 2020-02-16 19:49
+ */
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] tails = new int[nums.length];
+        int res = 0;
+        for (int num : nums) {
+            int i = 0, j = res;
+            while (i < j) {
+                int m = (i + j) / 2;
+                if (tails[m] < num) i = m + 1;
+                else j = m;
+            }
+            tails[i] = num;
+            if (res == j) res++;
+        }
+        return res;
+    }
+}
