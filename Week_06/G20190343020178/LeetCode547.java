0511aca8f31674017e5d7d00054e04a18ce82fe5
liangshenghe <ahe513@163.com>
2020-02-20 10:05:50 +0800

    0178-Week 06

---
+++ Week_06/G20190343020178/LeetCode547.java
@@ -0,0 +1,26 @@
+
+
+
+public class Solution {
+    public int findCircleNum(int[][] M) {
+        int[] visited = new int[M.length];
+        int count = 0;
+        Queue < Integer > queue = new LinkedList < > ();
+        for (int i = 0; i < M.length; i++) {
+            if (visited[i] == 0) {
+                queue.add(i);
+                while (!queue.isEmpty()) {
+                    int s = queue.remove();
+                    visited[s] = 1;
+                    for (int j = 0; j < M.length; j++) {
+                        if (M[s][j] == 1 && visited[j] == 0)
+                            queue.add(j);
+                    }
+                }
+                count++;
+            }
+        }
+        return count;
+    }
+}
+        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
